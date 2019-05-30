c80ca540e771a1016b7201b3d636d0716b977390
hillerc <chillbasslines@gmail.com>
2019-05-29 17:21:22 -0700

    fixed emptyBotId

--- tightrope_v1.js
+++ tightrope_v1.js
@@ -29,13 +29,13 @@ function setBrokerageId(brokerageId) {
 
 function init(config) {
   const host = config.host || ''
-  const botId = config.botId || ''
+  const botId = config.botId || 'learning'
   const brokerageId = config.brokerageId || ''
   const cssHref = host + '/assets/modules/channel-web/inject.css'
   injectDOMElement('link', 'head', { rel: 'stylesheet', href: cssHref })
 
   const options = encodeURIComponent(JSON.stringify({ config: config }))
-  const iframeSrc = host + '/lite/' + 'learning' + '/?m=channel-web&v=Embedded&options=' + options
+  const iframeSrc = host + '/lite/' + botId + '/?m=channel-web&v=Embedded&options=' + options
   const iframeHTML = '<iframe id="bp-widget" frameborder="0" src="' + iframeSrc + '" class="bp-widget-web"/>'
   injectDOMElement('div', 'body', { id: 'bp-web-widget', innerHTML: iframeHTML })
 
